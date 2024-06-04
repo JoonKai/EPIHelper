a59f63a3784e891cc74a6dc18fcb1d31261526fc
손준석 <joonseok831@gmail.com>
2024-06-04 22:18:44 +0900

    초기 커밋.

---
+++ WPF_Base/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using EPIHelper.Services;
+using EPIHelper.ViewModels;
+using EPIHelper.Views;
+
+namespace EPIHelper
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IServiceProvider _services = null;
+        public App()
+        {
+            _services = ConfigureService();
+        }
+
+        private IServiceProvider ConfigureService()
+        {
+            IServiceCollection services = new ServiceCollection();
+            //View
+            services.AddSingleton<MainView>();
+            services.AddTransient<SettingView>();
+            //ViewModel
+            services.AddSingleton<MainViewModel>();
+            services.AddTransient<SettingViewModel>();
+            //Service
+            services.AddSingleton<IViewService, ViewService>();
+
+
+            return services.BuildServiceProvider();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var viewService = (IViewService)_services.GetService(typeof(IViewService));
+            viewService.ShowMainView();
+            
+        }
+    }
+}
---
+++ WPF_Base/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using EPIHelper.Commands;
+using EPIHelper.Services;
+using System.Windows.Input;
+
+namespace EPIHelper.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IViewService _viewService;
+        public MainViewModel(IViewService viewService)
+        {
+            _viewService = viewService;
+        }
+        public ICommand ShowSettingViewCommand => new RelayCommand<object>(ShowSettingView);
+
+        private void ShowSettingView(object obj)
+        {
+            _viewService.ShowSettingView();
+        }
+    }
+}
