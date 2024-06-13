ce90c9767b97b72422e1e3fb835b4f1865c1a697
손준석 <joonseok831@gmail.com>
2024-06-14 02:04:51 +0900

    1111

---
+++ EPIControls/Controls/BaseCustomControl/EPICombobox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EPIControls.Controls.BaseCustomControl
+{
+    public class EPICombobox : ComboBox
+    {
+        static EPICombobox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EPICombobox), new FrameworkPropertyMetadata(typeof(EPICombobox)));
+        }
+    }
+}
---
+++ EPIControls/Controls/BaseCustomControl/EPISlider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EPIControls.Controls.BaseCustomControl
+{
+    public class EPISlider : Slider
+    {
+        static EPISlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EPISlider), new FrameworkPropertyMetadata(typeof(EPISlider)));
+        }
+    }
+}
